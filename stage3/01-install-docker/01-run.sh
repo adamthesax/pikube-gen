f7cfe560c49398bd8c1d0449e21cd31b2c4f09e9
Adam Sax <adam@rewiredpictures.com>
2020-03-12 21:52:23 -0400

    migrate bootstrapping to primary be in the config payload

--- stage3/01-install-docker/01-run.sh
+++ stage3/01-install-docker/01-run.sh
@@ -5,4 +5,7 @@ install -m 644 files/daemon.json "${ROOTFS_DIR}/etc/docker/daemon.json"
 
 on_chroot << EOF
 usermod -aG docker $FIRST_USER_NAME
+dphys-swapfile swapoff
+dphys-swapfile uninstall
+systemctl disable dphys-swapfile
 EOF
